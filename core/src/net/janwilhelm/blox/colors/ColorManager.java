7dbaf9f8ea40b0fbb91e558d624f188895e1eb64
Jan Wilhelm <jan.b.h.wilhelm@gmx.de>
2018-05-02 21:40:27 +0200

    Added comments

--- core/src/net/janwilhelm/blox/colors/ColorManager.java
+++ core/src/net/janwilhelm/blox/colors/ColorManager.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.graphics.Color;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * Responsible for managing all the color related functions needed for the game.
+ */
 public class ColorManager {
 
     private final ArrayList<GameColor> colors = new ArrayList<GameColor>();
@@ -12,11 +15,14 @@ public class ColorManager {
     private final Random random = new Random();
 
     public ColorManager() {
+
+        // Create the needed colors manually and assign their names
         this.colors.add(new GameColor("red", getColorFrom255RGB(255,37,37)));
         this.colors.add(new GameColor("yellow", getColorFrom255RGB(255,222,0)));
         this.colors.add(new GameColor("blue", getColorFrom255RGB(0,45,211)));
         this.colors.add(new GameColor("green", getColorFrom255RGB(0,218,46)));
 
+        // initialize the class by settings its first color
         this.refreshColor();
     }
 
@@ -24,15 +30,18 @@ public class ColorManager {
         return activeColor;
     }
 
+    /**
+     * Set the ColorManager#activeColor to a new randomly chosen
+     * color until it's different from the most recent color
+     */
     public void refreshColor() {
-        final GameColor lastColor = this.activeColor;
-        int newIndex = this.getRandomIndex();
-        GameColor newColor = lastColor;
+        int newIndex;
+        GameColor newColor;
 
         do {
             newIndex = getRandomIndex();
             newColor = this.colors.get(newIndex);
-        } while (newColor.equals(lastColor));
+        } while (newColor.equals(this.activeColor));
 
         this.activeColor = newColor;
     }
@@ -41,6 +50,14 @@ public class ColorManager {
         return random.nextInt(this.colors.size());
     }
 
+    /**
+     * Convenience method to get a Color object from 0-255 rgb values with
+     * an alpha value of 1 (255)
+     * @param red the red component of the color
+     * @param green the green component of the color
+     * @param blue the blue component of the color
+     * @return The Color object created from the rgb values
+     */
     public static Color getColorFrom255RGB(float red, float green, float blue) {
         return getColorFromRGB(red / 255F, green / 255F, blue / 255F, 1);
     }
